3ed9e5ffd885dbbc79d5b45264e41b0a8ebdc646
mattyan1053 <h.matsumaruka14161@gmail.com>
2019-03-28 01:20:46 +0900

    不可も修得単位数に数えられてしまう不具合を修正

--- gpa.js
+++ gpa.js
@@ -3,7 +3,7 @@ javascript:(
         var i;
         var S=0,A=0,B=0,C=0,D=0,other=0;
         var gpaSum = 0, effectiveGpaSum = 0;
-        var unitSum = 0, effectiveUnitSum = 0,unitSumAll = 0;
+        var unitSum = 0, effectiveUnitSum = 0,unitSumAll = 0, effectiveD = 0;
         var ifrmDoc = document.getElementById("main-frame-if");
         var doc = ifrmDoc.contentWindow.document;
         table = doc.getElementsByTagName("table").item(1).children[1];
@@ -13,7 +13,10 @@ javascript:(
             var isEffective = (table.rows[i].cells[1].innerText != "その他");
             if(record == '不可') {
                 gpaSum += 0;
-                if(isEffective) effectiveGpaSum += 0;
+                if(isEffective) {
+                    effectiveGpaSum += 0;
+                    effectiveD += unitNum;
+                }
                 D += unitNum;
             }
             else if(record == '可') {
@@ -44,6 +47,6 @@ javascript:(
             unitSumAll += unitNum;
             if(isEffective) effectiveUnitSum += unitNum;
         }
-        alert("修得科目数: " + table.rows.length + "\n" + "修得単位数: " + unitSumAll + " (うち卒業要件に認められる単位数:" + effectiveUnitSum + ")" + "\n" + "秀:" + S + " 優:" + A + " 良:" + B + " 可:" + C + " 不可:" + D + " その他:" + other + "\n" + "GPA: " + (gpaSum / unitSum).toFixed(2) + " (うち卒業要件単位のGPA:" + (effectiveGpaSum / effectiveUnitSum).toFixed(2) + ")");
+        alert("修得科目数: " + table.rows.length + "\n" + "修得単位数: " + (unitSumAll - D) + " (うち卒業要件に認められる単位数:" + (effectiveUnitSum - effectiveD) + ")" + "\n" + "秀:" + S + " 優:" + A + " 良:" + B + " 可:" + C + " 不可:" + D + " その他:" + other + "\n" + "GPA: " + (gpaSum / unitSum).toFixed(2) + " (うち卒業要件単位のGPA:" + (effectiveGpaSum / effectiveUnitSum).toFixed(2) + ")");
     }
 )();
